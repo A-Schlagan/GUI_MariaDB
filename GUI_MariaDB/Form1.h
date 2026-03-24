ae6b13d467b602907864337a7ad4c8330c930698
A-Schlagan <Alexander.schlagan@gmail.com>
2026-03-24 11:07:50 +0100

    upd: changebtn

--- GUI_MariaDB/Form1.h
+++ GUI_MariaDB/Form1.h
@@ -58,6 +58,8 @@ namespace CppCLRWinFormsProject {
 	private: System::Windows::Forms::TextBox^ txt_maxID;
 
 	private: System::Windows::Forms::Label^ lbl_von;
+	private: System::Windows::Forms::Button^ btn_auslesen;
+	private: System::Windows::Forms::Button^ btn_aendern;
 
 
 		   /// <summary>
@@ -83,25 +85,28 @@ namespace CppCLRWinFormsProject {
 			this->txt_ID2 = (gcnew System::Windows::Forms::TextBox());
 			this->txt_maxID = (gcnew System::Windows::Forms::TextBox());
 			this->lbl_von = (gcnew System::Windows::Forms::Label());
+			this->btn_auslesen = (gcnew System::Windows::Forms::Button());
+			this->btn_aendern = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// txt_ID
 			// 
-			this->txt_ID->Location = System::Drawing::Point(343, 67);
+			this->txt_ID->Location = System::Drawing::Point(340, 182);
 			this->txt_ID->Name = L"txt_ID";
+			this->txt_ID->ReadOnly = true;
 			this->txt_ID->Size = System::Drawing::Size(153, 20);
 			this->txt_ID->TabIndex = 1;
 			// 
 			// txt_Vorname
 			// 
-			this->txt_Vorname->Location = System::Drawing::Point(343, 112);
+			this->txt_Vorname->Location = System::Drawing::Point(340, 227);
 			this->txt_Vorname->Name = L"txt_Vorname";
 			this->txt_Vorname->Size = System::Drawing::Size(153, 20);
 			this->txt_Vorname->TabIndex = 2;
 			// 
 			// txt_Nachname
 			// 
-			this->txt_Nachname->Location = System::Drawing::Point(343, 162);
+			this->txt_Nachname->Location = System::Drawing::Point(340, 277);
 			this->txt_Nachname->Name = L"txt_Nachname";
 			this->txt_Nachname->Size = System::Drawing::Size(153, 20);
 			this->txt_Nachname->TabIndex = 3;
@@ -111,7 +116,7 @@ namespace CppCLRWinFormsProject {
 			this->lbl_ID->AutoSize = true;
 			this->lbl_ID->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->lbl_ID->Location = System::Drawing::Point(244, 67);
+			this->lbl_ID->Location = System::Drawing::Point(241, 182);
 			this->lbl_ID->Name = L"lbl_ID";
 			this->lbl_ID->Size = System::Drawing::Size(26, 20);
 			this->lbl_ID->TabIndex = 5;
@@ -122,7 +127,7 @@ namespace CppCLRWinFormsProject {
 			this->lbl_Vorname->AutoSize = true;
 			this->lbl_Vorname->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->lbl_Vorname->Location = System::Drawing::Point(244, 112);
+			this->lbl_Vorname->Location = System::Drawing::Point(241, 227);
 			this->lbl_Vorname->Name = L"lbl_Vorname";
 			this->lbl_Vorname->Size = System::Drawing::Size(74, 20);
 			this->lbl_Vorname->TabIndex = 6;
@@ -133,7 +138,7 @@ namespace CppCLRWinFormsProject {
 			this->lbl_Nachname->AutoSize = true;
 			this->lbl_Nachname->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->lbl_Nachname->Location = System::Drawing::Point(244, 160);
+			this->lbl_Nachname->Location = System::Drawing::Point(241, 275);
 			this->lbl_Nachname->Name = L"lbl_Nachname";
 			this->lbl_Nachname->Size = System::Drawing::Size(86, 20);
 			this->lbl_Nachname->TabIndex = 7;
@@ -141,7 +146,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			// btn_left
 			// 
-			this->btn_left->Location = System::Drawing::Point(248, 413);
+			this->btn_left->Location = System::Drawing::Point(239, 78);
 			this->btn_left->Name = L"btn_left";
 			this->btn_left->Size = System::Drawing::Size(50, 26);
 			this->btn_left->TabIndex = 8;
@@ -151,7 +156,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			// btn_right
 			// 
-			this->btn_right->Location = System::Drawing::Point(463, 413);
+			this->btn_right->Location = System::Drawing::Point(454, 78);
 			this->btn_right->Name = L"btn_right";
 			this->btn_right->Size = System::Drawing::Size(50, 26);
 			this->btn_right->TabIndex = 9;
@@ -163,7 +168,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->txt_ID2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->txt_ID2->Location = System::Drawing::Point(304, 413);
+			this->txt_ID2->Location = System::Drawing::Point(295, 78);
 			this->txt_ID2->Name = L"txt_ID2";
 			this->txt_ID2->ReadOnly = true;
 			this->txt_ID2->Size = System::Drawing::Size(61, 26);
@@ -174,7 +179,7 @@ namespace CppCLRWinFormsProject {
 			// 
 			this->txt_maxID->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->txt_maxID->Location = System::Drawing::Point(394, 413);
+			this->txt_maxID->Location = System::Drawing::Point(385, 78);
 			this->txt_maxID->Name = L"txt_maxID";
 			this->txt_maxID->ReadOnly = true;
 			this->txt_maxID->Size = System::Drawing::Size(63, 26);
@@ -186,18 +191,44 @@ namespace CppCLRWinFormsProject {
 			this->lbl_von->AutoSize = true;
 			this->lbl_von->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->lbl_von->Location = System::Drawing::Point(364, 419);
+			this->lbl_von->Location = System::Drawing::Point(355, 84);
 			this->lbl_von->Name = L"lbl_von";
 			this->lbl_von->Size = System::Drawing::Size(34, 20);
 			this->lbl_von->TabIndex = 12;
 			this->lbl_von->Text = L"von";
 			this->lbl_von->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
+			// btn_auslesen
+			// 
+			this->btn_auslesen->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn_auslesen->Location = System::Drawing::Point(12, 508);
+			this->btn_auslesen->Name = L"btn_auslesen";
+			this->btn_auslesen->Size = System::Drawing::Size(128, 52);
+			this->btn_auslesen->TabIndex = 13;
+			this->btn_auslesen->Text = L"Auslesen";
+			this->btn_auslesen->UseVisualStyleBackColor = true;
+			this->btn_auslesen->Click += gcnew System::EventHandler(this, &Form1::btn_auslesen_Click);
+			// 
+			// btn_aendern
+			// 
+			this->btn_aendern->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn_aendern->Location = System::Drawing::Point(146, 508);
+			this->btn_aendern->Name = L"btn_aendern";
+			this->btn_aendern->Size = System::Drawing::Size(124, 52);
+			this->btn_aendern->TabIndex = 14;
+			this->btn_aendern->Text = L"Aendern";
+			this->btn_aendern->UseVisualStyleBackColor = true;
+			this->btn_aendern->Click += gcnew System::EventHandler(this, &Form1::btn_aendern_Click);
+			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(799, 572);
+			this->Controls->Add(this->btn_aendern);
+			this->Controls->Add(this->btn_auslesen);
 			this->Controls->Add(this->lbl_von);
 			this->Controls->Add(this->txt_maxID);
 			this->Controls->Add(this->txt_ID2);
@@ -218,107 +249,117 @@ namespace CppCLRWinFormsProject {
 		}
 #pragma endregion
 	
-	private: void datensatz_auslesen(int gesuchteID) {
-		String^ sqltxt;
+	// Auslagerung des Datenbank-Commands 
+	private: MySqlDataReader^ cmdDB(String^ sqltxt) {
 		String^ constring = L"datasource=localhost;port=3306;username=root;password=DAA";
-		// connect zur DB
 		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
-		// sql über connect
-		sqltxt = "SELECT *, "
-        "(SELECT COUNT(ID) FROM sql_database_test.tbl_test) AS total_count, " // zählt alle vorhandenen Datensätze.
-        "(SELECT COUNT(ID) FROM sql_database_test.tbl_test WHERE ID <= @ID) AS row_num " //zählt, der wievielte Datensatz das ist (alle IDs <= der gesuchten ID)
-		"FROM sql_database_test.tbl_test WHERE ID = @ID;";
-
 		MySqlCommand^ cmdDataBase = gcnew MySqlCommand(sqltxt, conDataBase);
-		//reader
-		cmdDataBase->Parameters->AddWithValue("@ID", gesuchteID);
 		MySqlDataReader^ myReader;
-
 
 		try {
 			conDataBase->Open();
 			myReader = cmdDataBase->ExecuteReader();
-			if (myReader->Read()) {
-				this->txt_ID->Text = myReader->GetInt32(0).ToString();				
-				this->txt_Vorname->Text = myReader->GetString(1);
-				this->txt_Nachname->Text = myReader->GetString(2);
-
-				int rowNum = Convert::ToInt32(myReader["row_num"]);
-				int totalCount = Convert::ToInt32(myReader["total_count"]);
-				
-				this->txt_ID2->Text = rowNum.ToString();
-				this->txt_maxID->Text = totalCount.ToString();
-
-				this->btn_left->Enabled = (rowNum > 1);
-				this->btn_right->Enabled = (rowNum < totalCount);
-			}
+			return myReader;
 		}
 		catch (Exception^ ex) {
 			MessageBox::Show(ex->Message);
+			return nullptr;
 		}
-		finally {
-			if (conDataBase->State == ConnectionState::Open) {
-				conDataBase->Close();
-			}
-		}
+		
 	}
 
-	private: int naechsteID(int aktuelle_id, bool vorwaerts) {
-		int neueID = -1;
-		String^ constring = L"datasource=localhost;port=3306;username=root;password=DAA";
-		MySqlConnection^ conDataBase = gcnew MySqlConnection(constring);
+	private: void datensatz_auslesen() {
+		int datensatz;
 		String^ sqltxt;
+		String^ sqltxtmax;
 
-		if (vorwaerts) {
-			sqltxt = "SELECT MIN(ID) FROM sql_database_test.tbl_test WHERE ID>@ID;";
-		}
-		else {
-			sqltxt = "SELECT MAX(ID) FROM sql_database_test.tbl_test WHERE ID<@ID;";
-		}
+		datensatz = int::Parse(this->txt_ID2->Text);
 
-		MySqlCommand^ cmdDataBase = gcnew MySqlCommand(sqltxt, conDataBase);
-		cmdDataBase->Parameters->AddWithValue("@ID", aktuelle_id);
+		sqltxt = "SELECT * FROM sql_database_test.tbl_test LIMIT " + (datensatz - 1) + ", 1; ";
+		sqltxtmax = "SELECT COUNT(ID) FROM sql_database_test.tbl_test;";
+
+		MySqlDataReader^ myReader;
 
 		try {
-			conDataBase->Open();
-			Object^ result = cmdDataBase->ExecuteScalar();
-			// Prüfen, ob eine ID gefunden wurde 
-			if (result != nullptr && result != DBNull::Value) {
-				neueID = Convert::ToInt32(result);
+			// Maximum auslesen und in txt_maxID schreiben
+			myReader = cmdDB(sqltxtmax);
+			if (myReader != nullptr && myReader->Read()) {
+				this->txt_maxID->Text = myReader->GetInt32(0).ToString();
 			}
+			// Reader schließen
+			if (myReader != nullptr) myReader->Close();
+
+			// Datensatz holen
+			myReader = cmdDB(sqltxt);
+			while (myReader != nullptr && myReader->Read()) {
+				this->txt_ID->Text = myReader->GetInt32(0).ToString();
+				this->txt_Vorname->Text = myReader->GetString(1);
+				this->txt_Nachname->Text = myReader->GetString(2);
+			}
+			if (myReader != nullptr) myReader->Close();
 		}
 		catch (Exception^ ex) {
 			MessageBox::Show(ex->Message);
 		}
-		finally {
-			conDataBase->Close();
-		}
-		return neueID;
 	}
 
 	private: System::Void btn_left_Click(System::Object^ sender, System::EventArgs^ e) {
-		int prevID = naechsteID(aktuelleID, false);
-		if (prevID !=-1){
-			aktuelleID=prevID;
-			datensatz_auslesen(aktuelleID);
+		int datensatz;
+		datensatz = int::Parse(this->txt_ID2->Text);
+		datensatz--; // Einen Datensatz zurück
+
+		if (datensatz < 1) {
+			MessageBox::Show("Sie sind beim ersten Datensatz!");
+			datensatz = 1; // Bleibt bei 1
 		}
+
+		this->txt_ID2->Text = datensatz.ToString();
+		datensatz_auslesen();
 	}
+
 	private: System::Void btn_right_Click(System::Object^ sender, System::EventArgs^ e) {
-		int nextID = naechsteID(aktuelleID, true);
+		int datensatz, datensatzmax;
+		datensatz = int::Parse(this->txt_ID2->Text);
+		datensatzmax = int::Parse(this->txt_maxID->Text);
+		datensatz++; // Einen Datensatz vor
 
-		if (nextID != -1) {
-			aktuelleID = nextID;
-			datensatz_auslesen(aktuelleID);
-		}		
+		if (datensatz > datensatzmax) {
+			MessageBox::Show("Sie sind beim letzten Datensatz!");
+			datensatz--; // Setzt den Zähler wieder zurück auf das Maximum
+		}
+
+		this->txt_ID2->Text = datensatz.ToString();
+		datensatz_auslesen();
 	}
-	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
-		int ersteID = naechsteID(0, true);
 
-		if (ersteID != -1) {
-			aktuelleID = ersteID;
-			datensatz_auslesen(aktuelleID);
+	// Beim Laden der Form
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+		// Startwerte setzen, damit .Parse() nicht abstürzt
+		this->txt_ID2->Text = "1";
+		this->txt_maxID->Text = "1";
+
+		datensatz_auslesen();
+	}
+
+	private: System::Void btn_auslesen_Click(System::Object^ sender, System::EventArgs^ e) {
+		datensatz_auslesen();
+	}
+
+	private: System::Void btn_aendern_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ id = this->txt_ID->Text;
+		String^ neuerVorname = this->txt_Vorname->Text;
+		String^ neuerNachname = this->txt_Nachname->Text;
+
+		String^ sqltxt = "UPDATE sql_database_test.tbl_test SET Vorname = '" + neuerVorname + "', Nachname = '" + neuerNachname + "' WHERE ID = " + id + ";";
+
+		MySqlDataReader^ myReader = cmdDB(sqltxt);//Methode aufrufen
+
+		if (myReader != nullptr) {
+			MessageBox::Show("Datensatz erfolgreich geändert!");
+
+			// Ansicht aktualisieren
+			datensatz_auslesen();
 		}
 	}
-
 };
 }
